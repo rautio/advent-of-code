5656e75b80b5004fa17c8dbcfcaec425d28dadd7
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2023-12-09 16:40:26 -0800

    Day 2 part 1

---
+++ 2023/day03/src/main.rs
@@ -0,0 +1,68 @@
+use regex::Regex;
+use std::fs::read_to_string;
+
+#[derive(Debug)]
+struct CubeSet {
+    red: u32,
+    blue: u32,
+    green: u32,
+}
+
+fn is_game_valid(sets: Vec<CubeSet>, max: CubeSet) -> bool {
+    for set in sets {
+        if set.red > max.red || set.green > max.green || set.blue > max.blue {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn parse_sets(line: &str) -> Vec<CubeSet> {
+    let re: Regex = Regex::new(r"([0-9]+) (red|blue|green)").unwrap();
+    let mut cubes: Vec<CubeSet> = Vec::new();
+    for sets in line.split(';') {
+        let mut cube = CubeSet {
+            red: 0,
+            blue: 0,
+            green: 0,
+        };
+        for set in sets.split(',') {
+            let cap = re.captures(set).unwrap();
+            let count: u32 = cap.get(1).unwrap().as_str().parse().unwrap();
+            let color = cap.get(2).unwrap().as_str();
+            if color == "red" {
+                cube.red = count;
+            }
+            if color == "green" {
+                cube.green = count;
+            }
+            if color == "blue" {
+                cube.blue = count;
+            }
+        }
+        cubes.push(cube);
+    }
+    return cubes;
+}
+
+fn main() {
+    // Part 1
+    let mut sum = 0;
+    let id_re: Regex = Regex::new(r"^Game ([0-9]+): (.*)$").unwrap();
+    for line in read_to_string("./part1.txt").unwrap().lines() {
+        let cap = id_re.captures(line).unwrap();
+        let id: u32 = cap.get(1).unwrap().as_str().parse().unwrap();
+        let sets = cap.get(2).unwrap().as_str();
+        let cube_sets = parse_sets(&sets);
+        let max = CubeSet {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
+        let is_valid = is_game_valid(cube_sets, max);
+        if is_valid {
+            sum += id;
+        }
+    }
+    println!("Part 1: {0}", sum);
+}
