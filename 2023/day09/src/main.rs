5d285c25024e9ea8013d866b25bd219a0b592a2e
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2023-12-11 20:15:03 -0800

    Day 9 part 2

--- 2023/day09/src/main.rs
+++ 2023/day09/src/main.rs
@@ -23,19 +23,43 @@ fn find_next(nums: &Vec<i32>) -> i32 {
     return nums[&len - 1] + res;
 }
 
+fn find_prev(nums: &Vec<i32>) -> i32 {
+    let mut diffs: Vec<i32> = Vec::new();
+    let mut all_zero = true;
+    let mut diff = 0;
+    let len = nums.len();
+    for (i, n) in nums.into_iter().enumerate() {
+        if i == len - 1 {
+            continue;
+        }
+        diff = nums[i + 1] - n;
+        diffs.push(diff);
+        if diff != 0 {
+            all_zero = false;
+        }
+    }
+    if all_zero {
+        return nums[0];
+    }
+    let res = find_prev(&diffs);
+    return nums[0] - res;
+}
+
 fn main() {
     let mut now = Instant::now();
-    let mut sum = 0;
+    let mut next = 0;
+    let mut prev = 0;
     // Parse all the numbers and parts
     for line in read_to_string("./input.txt").unwrap().lines() {
         let mut nums: Vec<i32> = Vec::new();
         for (_i, n) in line.split(' ').enumerate() {
             nums.push(n.parse::<i32>().unwrap());
         }
-        sum += find_next(&nums);
+        next += find_next(&nums);
+        prev += find_prev(&nums);
     }
-    // Part 1
-    println!("Part 1: {}", sum);
+    println!("Part 1: {}", next);
+    println!("Part 2: {}", prev);
     let elapsed = now.elapsed();
     println!("Done in: {:.2?}!", elapsed);
 }
