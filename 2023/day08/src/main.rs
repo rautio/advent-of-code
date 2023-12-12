e09c8b5284199e56a58649ad6fb172a8e3e9d681
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2023-12-11 22:56:00 -0800

    Day 8 part 2

--- 2023/day08/src/main.rs
+++ 2023/day08/src/main.rs
@@ -1,14 +1,20 @@
+use num::integer::lcm;
 use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::time::Instant;
 
-fn find_steps(start: &String, instr: Vec<char>, map: HashMap<String, Vec<String>>) -> u32 {
+fn find_steps(
+    start: &String,
+    instr: &Vec<char>,
+    map: &HashMap<String, Vec<String>>,
+    use_full: bool,
+) -> u32 {
     let mut steps = 0;
     let mut idx = 0;
     let mut cur = start;
     loop {
         let i = idx % instr.len();
-        if cur == "ZZZ" {
+        if (!use_full && cur.ends_with("Z")) || (use_full && cur.ends_with("ZZZ")) {
             return steps;
         }
         if instr[i] == 'L' {
@@ -22,12 +28,26 @@ fn find_steps(start: &String, instr: Vec<char>, map: HashMap<String, Vec<String>
     }
 }
 
+fn find_sim_steps(
+    starts: Vec<String>,
+    instr: &Vec<char>,
+    map: &HashMap<String, Vec<String>>,
+) -> u64 {
+    let mut res: u64 = 1;
+    for s in starts {
+        let steps = find_steps(&s, instr, map, false);
+        res = lcm(res, steps as u64);
+    }
+    return res;
+}
+
 fn main() {
     let mut now = Instant::now();
 
     let mut map: HashMap<String, Vec<String>> = HashMap::new();
     let mut instr: Vec<char> = Vec::new();
-    let mut start = String::from("AAA");
+    let start = String::from("AAA");
+    let mut starts: Vec<String> = Vec::new();
 
     // Parse all the numbers and parts
     for (i, line) in read_to_string("./input.txt")
@@ -41,12 +61,18 @@ fn main() {
         }
         if i > 1 {
             let s = line[..3].to_string();
+            if s.ends_with("A") {
+                starts.push(s.clone());
+            }
             let l = line[7..10].to_string();
             let r = line[12..15].to_string();
             map.insert(s, vec![l, r]);
         }
     }
-    println!("Part 1: {}", find_steps(&start, instr, map));
+    println!("Part 1: {}", find_steps(&start, &instr, &map, true));
     let elapsed = now.elapsed();
     println!("Done in: {:.2?}!", elapsed);
+    now = Instant::now();
+    println!("Part 2: {}", find_sim_steps(starts, &instr, &map));
+    println!("Done in: {:.2?}!", now.elapsed());
 }
