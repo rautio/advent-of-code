21122df7f187c9bf13251c65448df665e7b0190a
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-12-02 15:10:03 -0800

    day 1 part 2

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,14 +1,27 @@
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::time::Instant;
 
-fn total_distance(mut left: Vec<i32>, mut right: Vec<i32>) -> i32 {
+fn total_distance(left: Vec<i32>, right: Vec<i32>) -> i32 {
     let mut sum = 0;
-    left.sort();
-    right.sort();
     for i in 0..left.len() {
         sum += (left[i] - right[i]).abs();
     }
     sum
+}
+
+fn similarity_score(left: Vec<i32>, right: Vec<i32>) -> i32 {
+    let mut score = 0;
+    let mut right_occurences = HashMap::new();
+    for i in 0..right.len() {
+        *right_occurences.entry(right[i]).or_insert(0) += 1;
+    }
+    for i in 0..left.len() {
+        if right_occurences.contains_key(&left[i]) {
+            score += left[i] * right_occurences.get(&left[i]).unwrap();
+        }
+    }
+    score
 }
 
 fn main() {
@@ -21,7 +34,11 @@ fn main() {
         left.push(splits[0].parse::<i32>().unwrap());
         right.push(splits[splits.len() - 1].parse::<i32>().unwrap());
     }
-    println!("Part 1: {}", total_distance(left, right));
+    left.sort();
+    right.sort();
+    println!("Part 1: {}", total_distance(left.clone(), right.clone()));
     println!("Done in: {:?}!", now.elapsed());
     now = Instant::now();
+    println!("Part 2: {}", similarity_score(left, right));
+    println!("Done in: {:?}!", now.elapsed());
 }
