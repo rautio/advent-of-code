25fe2d71e60cb311df316c326ce36157e6cd5155
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-12-06 08:46:46 -0800

    day 6 part 2

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -8,12 +8,64 @@ struct Pt {
     y: i32,
 }
 
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+struct Dir {
+    pt: Pt,
+    dir: Pt,
+}
+
 const N: Pt = Pt { x: 0, y: -1 };
 const E: Pt = Pt { x: 1, y: 0 };
 const S: Pt = Pt { x: 0, y: 1 };
 const W: Pt = Pt { x: -1, y: 0 };
 
-fn count_path(grid: &HashMap<Pt, char>, start: &Pt) -> usize {
+fn is_loop(grid: &HashMap<Pt, char>, start: &Pt) -> bool {
+    let dirs = vec![N, E, S, W];
+    let mut dir_idx = 0; // Always start moving up
+    let mut cur = start.clone();
+    let mut visited: HashMap<Dir, bool> = HashMap::new();
+
+    while grid.contains_key(&cur) {
+        let dir = dirs[dir_idx % dirs.len()];
+        visited.insert(Dir { pt: cur, dir }, true);
+        let next = Pt {
+            x: cur.x + dir.x,
+            y: cur.y + dir.y,
+        };
+        if grid.contains_key(&next) && *grid.get(&next).unwrap() == '#' {
+            // Change direction
+            dir_idx += 1;
+        } else {
+            cur = next;
+        }
+        if visited.contains_key(&Dir {
+            pt: cur,
+            dir: dirs[dir_idx % dirs.len()],
+        }) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn count_possible_loops(grid: &HashMap<Pt, char>, visited: &HashMap<Pt, bool>, start: &Pt) -> i32 {
+    let mut count = 0;
+
+    for pt in visited.keys() {
+        if pt != start {
+            let mut grid_copy = grid.clone();
+            grid_copy.insert(*pt, '#');
+            if is_loop(&grid_copy, start) {
+                count += 1;
+            }
+        }
+    }
+
+    count
+}
+
+fn find_path(grid: &HashMap<Pt, char>, start: &Pt) -> HashMap<Pt, bool> {
     let dirs = vec![N, E, S, W];
     let mut visited: HashMap<Pt, bool> = HashMap::new();
     let mut dir_idx = 0; // Always start moving up
@@ -34,7 +86,7 @@ fn count_path(grid: &HashMap<Pt, char>, start: &Pt) -> usize {
         }
     }
 
-    visited.keys().len()
+    visited
 }
 
 fn main() {
@@ -57,10 +109,11 @@ fn main() {
         y += 1;
     }
     // Part 1
-    println!("Part 1: {}", count_path(&grid, &start));
+    let path = find_path(&grid, &start);
+    println!("Part 1: {}", path.keys().len());
     println!("Done in: {:?}!", now.elapsed());
     // Part 2
     now = Instant::now();
-    println!("Part 2: {}", 0);
+    println!("Part 2: {}", count_possible_loops(&grid, &path, &start));
     println!("Done in: {:?}!", now.elapsed());
 }
