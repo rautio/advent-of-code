22ee4ee839a6c8ea232347261ad2ecaa2a65801c
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-12-07 11:27:53 -0800

    Day 7 part 2

--- 2024/day07/src/main.rs
+++ 2024/day07/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::fs::read_to_string;
 use std::time::Instant;
 
@@ -5,36 +6,56 @@ use std::time::Instant;
 struct Eq {
     result: i64,
     numbers: Vec<i64>,
+    total: i64,
 }
 
-fn could_be_true(eq: &Eq) -> bool {
+fn could_be_true(eq: &Eq, concatenate: bool) -> bool {
     let result = eq.result;
     let mut nums = eq.numbers.clone();
+    let total = eq.total;
     if nums.len() == 0 {
-        return result == 0;
+        return result == total;
     }
-    if nums.len() == 1 {
-        return result == nums[0];
-    }
-    // Can add or multiply
-    let last = nums.pop().unwrap();
+    let next = nums.remove(0);
     let add = Eq {
-        result: result - last,
+        result: result,
         numbers: nums.clone(),
+        total: total + next,
     };
-    let is_divisible = result % last == 0; // If not the number gets cast to i64 anyway
+    let mut mul_total = total;
+    if mul_total == 0 {
+        mul_total = 1;
+    }
     let mul = Eq {
-        result: result / last,
+        result: result,
         numbers: nums.clone(),
+        total: mul_total * next,
     };
-    return could_be_true(&add) || (is_divisible && could_be_true(&mul));
+    let mut concat_possible = false;
+    if concatenate && total > 0 {
+        let concat_nums = nums.clone();
+        let new_num = vec![total.to_string(), next.to_string()]
+            .join("")
+            .parse::<i64>()
+            .unwrap();
+
+        let mut new_nums: VecDeque<i64> = VecDeque::from(concat_nums);
+        new_nums.push_front(new_num);
+        let concat = Eq {
+            result,
+            numbers: Vec::from(new_nums),
+            total: 0,
+        };
+        concat_possible = could_be_true(&concat, concatenate);
+    }
+    return could_be_true(&add, concatenate) || could_be_true(&mul, concatenate) || concat_possible;
 }
 
-fn count_true(eqs: &Vec<Eq>) -> i64 {
+fn count_true(eqs: &Vec<Eq>, concatenate: bool) -> i64 {
     let mut count = 0;
 
     for eq in eqs {
-        if could_be_true(eq) {
+        if could_be_true(eq, concatenate) {
             count += eq.result;
         }
     }
@@ -59,13 +80,14 @@ fn main() {
         eqs.push(Eq {
             result,
             numbers: nums,
+            total: 0,
         })
     }
     // Part 1
-    println!("Part 1: {}", count_true(&eqs));
+    println!("Part 1: {}", count_true(&eqs, false));
     println!("Done in: {:?}!", now.elapsed());
     // Part 2
     now = Instant::now();
-    println!("Part 2: {}", 0);
+    println!("Part 2: {}", count_true(&eqs, true));
     println!("Done in: {:?}!", now.elapsed());
 }
