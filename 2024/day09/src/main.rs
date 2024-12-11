0dd0f37439b4bfc7442a16e6200bff2c28560afa
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-12-11 09:58:41 -0800

    Refactor day 9 part 2

--- 2024/day09/src/main.rs
+++ 2024/day09/src/main.rs
@@ -13,6 +13,12 @@ struct FileBlock {
     mem: Memory,
     index: usize,
 }
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+struct Block {
+    id: i32,
+    len: i32,
+    is_free: bool,
+}
 
 fn print_memory(mem: &Vec<Memory>) {
     let mut res = String::new();
@@ -40,52 +46,40 @@ fn sort_memory(mem: &mut Vec<Memory>) {
     }
 }
 
-fn sort_files(mem: &mut Vec<Memory>) {
-    let mut cur_file: Vec<FileBlock> = Vec::new();
-    let mut moved: HashMap<i32, bool> = HashMap::new();
-    let mut cur_id = -1; // Starting at the right guarantees non-zero starting id
-    for i in 1..=mem.len() {
-        // If new index or free, check if we have a file
-        let cur_index = mem.len() - i;
-        let cur = mem[cur_index];
-        if (cur.is_free || cur.id != cur_id) && cur_file.len() > 0 {
-            // We have a block, check for matches.
-            if cur_file.len() > 0 && !moved.contains_key(&cur_id) {
-                let mut free_space: Vec<FileBlock> = Vec::new();
-                for j in 0..cur_index + 1 {
-                    let block = mem[j];
-                    if block.is_free {
-                        free_space.push(FileBlock {
-                            mem: block,
-                            index: j,
-                        });
-                    } else {
-                        if free_space.len() > 0 && free_space.len() >= cur_file.len() {
-                            for c in 0..cur_file.len() {
-                                let cur_idx = cur_file[c].index;
-                                let free_idx = free_space[c].index;
-                                if free_idx >= cur_idx {
-                                    break;
-                                }
-                                mem.swap(free_idx, cur_idx);
-                            }
-                            break;
-                        }
-                        free_space = Vec::new();
-                    }
+fn sort_disk(disk: &mut Vec<Block>) {
+    let mut rpointer = disk.len() - 1;
+    while rpointer > 0 {
+        let mut lpointer = 0;
+        let right = disk[rpointer];
+        if (!right.is_free) {
+            while lpointer < rpointer {
+                let left = disk[lpointer];
+                // Fill and replace
+                if left.len > right.len {
+                    disk[rpointer] = Block {
+                        id: 0,
+                        is_free: true,
+                        len: right.len,
+                    };
+                    disk[lpointer] = right;
+                    disk.insert(
+                        lpointer,
+                        Block {
+                            id: 0,
+                            is_free: true,
+                            len: left.len - right.len,
+                        },
+                    );
+                    rpointer += 1; // We expanded the length
                 }
-                moved.insert(cur_id, true);
+                // Swap
+                if left.len == right.len {
+                    disk.swap(rpointer, lpointer);
+                }
+                lpointer += 1;
             }
-            // Reset block
-            cur_file = Vec::new();
         }
-        if !cur.is_free {
-            cur_file.push(FileBlock {
-                mem: cur,
-                index: cur_index,
-            });
-            cur_id = cur.id;
-        }
+        rpointer -= 1
     }
 }
 
@@ -101,9 +95,25 @@ fn checksum(mem: &Vec<Memory>) -> i64 {
     sum
 }
 
+fn checksum_disk(disk: &Vec<Block>) -> i64 {
+    let mut mem: Vec<Memory> = Vec::new();
+    for (i, f) in disk.into_iter().enumerate() {
+        for j in 0..f.len {
+            mem.push(Memory {
+                id: f.id,
+                is_free: f.is_free,
+            });
+        }
+    }
+
+    checksum(&mem)
+}
+
 fn main() {
     let mut now = Instant::now();
     let mut memory1: Vec<Memory> = Vec::new();
+    let mut files: Vec<File> = Vec::new();
+    let mut disk: Vec<Block> = Vec::new();
     for line in read_to_string("./src/input.txt").unwrap().lines() {
         let mut id = 0;
         for (i, c) in line.chars().into_iter().enumerate() {
@@ -114,6 +124,11 @@ fn main() {
                     is_free: i % 2 != 0,
                 })
             }
+            disk.push(Block {
+                id,
+                len: count,
+                is_free: i % 2 != 0,
+            });
             if i % 2 == 0 {
                 // File
                 id += 1;
@@ -127,7 +142,7 @@ fn main() {
     println!("Done in: {:?}!", now.elapsed());
     // Part 2
     now = Instant::now();
-    sort_files(&mut memory2);
-    println!("Part 2: {}", checksum(&memory2));
+    sort_disk(&mut disk);
+    println!("Part 2: {}", checksum_disk(&disk));
     println!("Done in: {:?}!", now.elapsed());
 }
