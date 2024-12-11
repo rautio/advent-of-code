69b1476eb351e2feb769afa4c84ef37a93ac9b50
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-12-11 08:52:23 -0800

    day 10 part 2

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -1,53 +1,72 @@
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::time::Instant;
 
-fn blink(stones: Vec<i64>) -> Vec<i64> {
-    let mut res = stones.clone();
-    let mut i = 0;
-    while i < res.len() {
-        let stone = res[i];
-        let stone_str = stone.to_string();
-        if stone == 0 {
-            res[i] = 1;
-        } else if stone_str.len() % 2 == 0 {
-            let (carve1, carve2) = stone_str.split_at(stone_str.len() / 2);
-            res[i] = carve1.parse::<i64>().unwrap();
-            res.insert(i + 1, carve2.parse::<i64>().unwrap());
-            i += 1;
-        } else {
-            res[i] = stone * 2024;
-        }
-        i += 1;
+fn blink_once(stone: usize) -> Vec<usize> {
+    if stone == 0 {
+        return vec![1];
     }
+    let stone_str = stone.to_string();
+    if stone_str.len() % 2 == 0 {
+        let (carve1, carve2) = stone_str.split_at(stone_str.len() / 2);
+        return vec![
+            carve1.parse::<usize>().unwrap(),
+            carve2.parse::<usize>().unwrap(),
+        ];
+    }
+    vec![stone * 2024]
+}
+
+fn blink_all(stones: HashMap<usize, usize>) -> HashMap<usize, usize> {
+    let mut res: HashMap<usize, usize> = HashMap::new();
+
+    for stone in stones {
+        let new_stones = blink_once(stone.0);
+        for new_stone in new_stones {
+            res.entry(new_stone)
+                .and_modify(|count| *count += stone.1)
+                .or_insert(stone.1);
+        }
+    }
+
     res
 }
 
-fn blink_times(stones: Vec<i64>, num_blinks: i64) -> Vec<i64> {
-    let mut new_stones = stones.clone();
-
+fn blink_times(stones: HashMap<usize, usize>, num_blinks: usize) -> HashMap<usize, usize> {
+    let mut freqs = stones.clone();
     for _ in 0..num_blinks {
-        new_stones = blink(new_stones);
+        freqs = blink_all(freqs);
     }
-
-    new_stones
+    freqs
 }
 
 fn main() {
     let mut now = Instant::now();
-    let mut stones: Vec<i64> = Vec::new();
+    let mut stones: Vec<usize> = Vec::new();
     for line in read_to_string("./src/input.txt").unwrap().lines() {
         stones = line
             .split_whitespace()
             .collect::<Vec<&str>>()
             .into_iter()
-            .map(|n| n.parse::<i64>().unwrap())
-            .collect::<Vec<i64>>();
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
     }
+    let freqs: HashMap<usize, usize> =
+        stones
+            .iter()
+            .copied()
+            .fold(HashMap::default(), |mut map, val| {
+                map.entry(val).and_modify(|count| *count += 1).or_insert(1);
+                map
+            });
     // Part 1
-    println!("Part 1: {}", blink_times(stones.clone(), 25).len());
+    println!(
+        "Part 1: {}",
+        blink_times(freqs.clone(), 25).values().sum::<usize>()
+    );
     println!("Done in: {:?}!", now.elapsed());
     // Part 2
     now = Instant::now();
-    println!("Part 2: {}", 0);
+    println!("Part 2:{}", blink_times(freqs, 75).values().sum::<usize>());
     println!("Done in: {:?}!", now.elapsed());
 }
