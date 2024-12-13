e1919ad1bd46c7dc659c9985e4c5e44c57ca43d7
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-12-12 21:45:30 -0800

    Dont use hardcoded max

--- 2024/day13/src/main.rs
+++ 2024/day13/src/main.rs
@@ -1,12 +1,13 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::cmp;
 use std::fs::read_to_string;
 use std::time::Instant;
 
 #[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
 struct Pt {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 #[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
@@ -16,17 +17,21 @@ struct Game {
     prize: Pt,
 }
 
-fn play_game(game: &Game) -> (bool, i32) {
-    let max = 100;
+fn play_game(game: &Game) -> (bool, i64) {
+    let a = game.a;
+    let b = game.b;
+    let prize = game.prize;
+    let max_a = cmp::max(prize.x / a.x, prize.y / a.y) + 1;
+    let max_b = cmp::max(prize.x / b.x, prize.y / b.y) + 1;
     // A costs 3 tokens
     // B costs 1 tokens
-    let mut tokens = 3 * max + max;
+    let mut tokens = 3 * max_a + max_b;
     let mut can_win = false;
-    for i in 0..max {
-        for j in 0..max {
-            let x = game.a.x * i + game.b.x * j;
-            let y = game.a.y * i + game.b.y * j;
-            if x == game.prize.x && y == game.prize.y {
+    for i in 0..max_a {
+        for j in 0..max_b {
+            let x = a.x * i + b.x * j;
+            let y = a.y * i + b.y * j;
+            if x == prize.x && y == prize.y {
                 can_win = true;
                 let cost = 3 * i + j;
                 if cost < tokens {
@@ -38,7 +43,7 @@ fn play_game(game: &Game) -> (bool, i32) {
     (can_win, tokens)
 }
 
-fn play_games(games: &Vec<Game>) -> (i32, usize) {
+fn play_games(games: &Vec<Game>) -> (i64, usize) {
     let mut tokens = 0;
     let mut prizes = 0;
 
@@ -61,6 +66,7 @@ fn main() {
     }
     let mut now = Instant::now();
     let mut games: Vec<Game> = Vec::new();
+    let mut harder_games: Vec<Game> = Vec::new();
     let mut i = 1;
     let mut a = Pt { x: 0, y: 0 };
     let mut b = Pt { x: 0, y: 0 };
@@ -69,16 +75,16 @@ fn main() {
             1 => {
                 let cap = reA.captures(line).unwrap();
                 a = Pt {
-                    x: cap[1].parse::<i32>().unwrap(),
-                    y: cap[2].parse::<i32>().unwrap(),
+                    x: cap[1].parse::<i64>().unwrap(),
+                    y: cap[2].parse::<i64>().unwrap(),
                 };
                 i = 2;
             }
             2 => {
                 let cap = reB.captures(line).unwrap();
                 b = Pt {
-                    x: cap[1].parse::<i32>().unwrap(),
-                    y: cap[2].parse::<i32>().unwrap(),
+                    x: cap[1].parse::<i64>().unwrap(),
+                    y: cap[2].parse::<i64>().unwrap(),
                 };
                 i = 3;
             }
@@ -88,8 +94,16 @@ fn main() {
                     a,
                     b,
                     prize: Pt {
-                        x: cap[1].parse::<i32>().unwrap(),
-                        y: cap[2].parse::<i32>().unwrap(),
+                        x: cap[1].parse::<i64>().unwrap(),
+                        y: cap[2].parse::<i64>().unwrap(),
+                    },
+                });
+                harder_games.push(Game {
+                    a,
+                    b,
+                    prize: Pt {
+                        x: cap[1].parse::<i64>().unwrap() + 10000000000000,
+                        y: cap[2].parse::<i64>().unwrap() + 10000000000000,
                     },
                 });
                 i = 4;
@@ -97,7 +111,7 @@ fn main() {
             4 => {
                 i = 1;
             }
-            i32::MIN..=0_i32 | 5_i32..=i32::MAX => todo!(),
+            i64::MIN..=0_i64 | 5_i64..=i64::MAX => todo!(),
         };
     }
     // Part 1
@@ -105,6 +119,6 @@ fn main() {
     println!("Done in: {:?}!", now.elapsed());
     // Part 2
     now = Instant::now();
-    println!("Part 2: {}", 0);
+    println!("Part 2: {}", play_games(&harder_games).0);
     println!("Done in: {:?}!", now.elapsed());
 }
