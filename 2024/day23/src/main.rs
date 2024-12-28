039f6e7d096b23a04393a63ed1e92461b552321e
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-12-27 20:09:12 -0800

    day 23 part 2

--- 2024/day23/src/main.rs
+++ 2024/day23/src/main.rs
@@ -4,7 +4,7 @@ use std::time::Instant;
 
 type Graph = HashMap<String, HashMap<String, String>>;
 
-fn solve_part1(graph: &Graph) -> i32 {
+fn find_triples(graph: &Graph) -> HashMap<Vec<String>, bool> {
     let mut triples: HashMap<Vec<String>, bool> = HashMap::new();
     for (first, v) in graph.iter() {
         for (second, _) in v.iter() {
@@ -25,13 +25,69 @@ fn solve_part1(graph: &Graph) -> i32 {
             }
         }
     }
+    triples
+}
+
+fn solve_part1(graph: &Graph) -> i32 {
     let mut count = 0;
+    let triples = find_triples(&graph);
     for (_, is_t) in triples {
         if is_t {
             count += 1;
         }
     }
     count
+}
+
+fn is_connected(start: String, end: String, graph: &Graph) -> bool {
+    graph.get(&start).unwrap().contains_key(&end) && graph.get(&end).unwrap().contains_key(&start)
+}
+
+fn max_clique(graph: &Graph) -> Vec<String> {
+    let triples: HashMap<Vec<String>, bool> = find_triples(&graph);
+    let mut cliques: Vec<Vec<String>> = Vec::new();
+    for (t, _) in triples.iter() {
+        cliques.push(t.to_vec());
+    }
+    let mut max: Vec<String> = Vec::new();
+    while cliques.len() > 0 {
+        let mut new_cliques: Vec<Vec<String>> = Vec::new();
+        let mut counted: HashMap<String, String> = HashMap::new();
+        for cur in cliques {
+            for (k, _) in graph.iter() {
+                let mut connected = true;
+                for c in &cur {
+                    // Has to be connected to every other element in cur
+                    if *c != *k {
+                        if !is_connected(c.clone(), k.clone(), graph) {
+                            connected = false;
+                            break;
+                        }
+                    }
+                }
+                if connected && !cur.contains(k) && !counted.contains_key(k) {
+                    counted.insert(k.to_string(), k.to_string());
+                    let mut new_cur = cur.clone();
+                    new_cur.push(k.clone());
+                    new_cliques.push(new_cur.clone());
+                    if new_cur.len() > max.len() {
+                        max = new_cur;
+                    }
+                    break;
+                }
+            }
+        }
+        cliques = new_cliques;
+    }
+
+    max
+}
+
+fn solve_part2(graph: &Graph) -> String {
+    let mut max = max_clique(graph);
+
+    max.sort();
+    max.iter().map(|x| x.to_string() + ",").collect::<String>()
 }
 
 fn main() {
@@ -56,7 +112,7 @@ fn main() {
     println!("Done in: {:?}!", now.elapsed());
     // Part 2
     now = Instant::now();
-    println!("Part 2: {}", 0);
+    println!("Part 2: {}", solve_part2(&graph));
     println!("Done in: {:?}!", now.elapsed());
 }
 
@@ -65,5 +121,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_foo() {}
+    fn test_is_connected() {
+        let c1: HashMap<String, String> = HashMap::from([
+            (String::from("ab"), String::from("ab")),
+            (String::from("ca"), String::from("ca")),
+        ]);
+        let c2: HashMap<String, String> = HashMap::from([
+            (String::from("tk"), String::from("tk")),
+            (String::from("ca"), String::from("ca")),
+        ]);
+        let c3: HashMap<String, String> = HashMap::from([(String::from("zk"), String::from("zk"))]);
+        let graph: Graph = HashMap::from([
+            (String::from("tk"), c1),
+            (String::from("ab"), c2),
+            (String::from("ca"), c3),
+        ]);
+        assert_eq!(
+            is_connected(String::from("tk"), String::from("ab"), &graph),
+            true
+        );
+        assert_eq!(
+            is_connected(String::from("tk"), String::from("ca"), &graph),
+            false
+        );
+    }
 }
